72c9b7cc8ba204e5ce372e85f8d84297a44c0482
BuildTools <unconfigured@null.spigotmc.org>
2021-09-26 19:14:54 +0800

    1.1加入菜单编写

--- src/main/java/rt/guirandomtp/GuiRandomTp.java
+++ src/main/java/rt/guirandomtp/GuiRandomTp.java
@@ -3,9 +3,11 @@ package rt.guirandomtp;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GuiRandomTp extends JavaPlugin {
+    public static JavaPlugin ins;
 
     @Override
     public void onEnable() {
+        ins = this;
         // Plugin startup logic
 
     }
---
+++ src/main/java/rt/guirandomtp/Menu.java
@@ -0,0 +1,11 @@
+package rt.guirandomtp;
+
+/**
+ * Author:NS
+ * date:2021/9/26 17:32
+ * Verious: 0.1
+ */
+public class Menu {
+
+
+}
