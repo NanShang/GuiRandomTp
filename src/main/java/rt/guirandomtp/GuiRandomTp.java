2d82c6c0fe9506d1229ab95424b59cb1926b562e
BuildTools <unconfigured@null.spigotmc.org>
2021-09-26 16:41:34 +0800

    Initial commit

---
+++ src/main/java/rt/guirandomtp/GuiRandomTp.java
@@ -0,0 +1,17 @@
+package rt.guirandomtp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GuiRandomTp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
